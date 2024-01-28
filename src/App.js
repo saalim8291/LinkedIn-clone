33da92f6ce9a26b55782765c99d790427c8e17ab
Saalim Bhatkar <saalimbhatkar786@gmail.com>
2024-01-28 11:44:02 +0530

    login page added

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Header from "./components/Header";
+// import Home from "./components/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route path="/home">
+            <Header />
+            {/* <Home /> */}
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
